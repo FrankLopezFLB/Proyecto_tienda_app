9fb64e33e2146fb5ff27922e0983f18e06347533
Green <franklopezflb@gmail.com>
2021-10-12 10:33:38 -0500

    0.0.1

---
+++ Proyecto_tienda_app/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_tienda_app
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Proyecto_tienda_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_tienda_app.MvcApplication" Language="C#" %>
