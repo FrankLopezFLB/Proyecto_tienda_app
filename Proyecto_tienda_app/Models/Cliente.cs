0cfd0fafeb4a1992c20b43da9a5c3ce84743184a
jossrc <kuro.rc10@gmail.com>
2021-10-25 14:31:47 -0500

    Creando modelo Cliente

---
+++ Proyecto_tienda_app/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_tienda_app.Models
+{
+    public class Cliente:Usuario
+    {
+
+    }
+}
