e0e74d4221fc329ee9d045bd8af4ab130eb3aec1
jossrc <kuro.rc10@gmail.com>
2021-10-25 14:37:47 -0500

    Creando archivo ClienteController

---
+++ Proyecto_tienda_app/Controllers/ClienteController.cs
@@ -0,0 +1,54 @@
+﻿using Proyecto_tienda_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_tienda_app.DAO
+{
+    public class ClienteController : Controller
+    {
+        // GET
+        public ActionResult Registrar()
+        {
+            Cliente nuevoCliente = new Cliente();
+
+            return View(nuevoCliente);
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Cliente cliente)
+        {
+            return View(cliente);
+        }
+
+        public ActionResult Listado()
+        {
+            List<Cliente> listado = new List<Cliente>();
+
+            return View();
+        }
+
+        // GET
+        public ActionResult Actualizar(int codigo)
+        {
+
+            // Se busca un cliente
+            Cliente cliente = new Cliente();
+
+            return View(cliente);
+        }
+
+        public ActionResult Actualizar(int codigo, Cliente cliente)
+        {
+            return View(cliente);
+        }
+
+        public ActionResult Eliminar(int codigo)
+        {
+            return View();
+        }
+
+    }
+}
